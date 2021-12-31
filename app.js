5e6207814d0796b586564ffb997605a9fd7aba35
liangming1 <liangming1@lightinthebox.com>
2021-12-31 15:11:57 +0800

    部署163music

--- app.js
+++ app.js
@@ -1,78 +1,145 @@
-const Koa = require('koa')
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const Koa_Session = require('koa-session');   // 导入koa-session  
-const cors = require('koa2-cors');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('./util/request')
+const packageJSON = require('./package.json')
+const exec = require('child_process').exec
+const cache = require('./util/apicache').middleware
+const { cookieToJson } = require('./util/index')
+const fileUpload = require('express-fileupload')
+const decode = require('safe-decode-uri-component')
 
-
-// 生成cookie
-const session_config = {
-  key: 'lm-cookie', /**  cookie的key。 (默认是 koa:sess) */
-  maxAge: { maxAge: 4000, httpOnly: true },   /**  session 过期时间，以毫秒ms为单位计算 。*/
-  autoCommit: true, /** 自动提交到响应头。(默认是 true) */
-  overwrite: true, /** 是否允许重写 。(默认是 true) */
-  httpOnly: true, /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */
-  signed: true, /** 是否签名。(默认是 true) */
-  rolling: true, /** 是否每次响应时刷新Session的有效期。(默认是 false) */
-  renew: false, /** 是否在Session快过期时刷新Session的有效期。(默认是 false) */
-};
-
-// 初始化
-const app = new Koa()
-
-//将路由文件引入
-const index = require('./routes/index');
-
-
-
-app.use(cors());
-
-// 启动mongos连接数据库
-const mongodb = require('./config/mongodb');
-mongodb.connect();
-
-// error handler
-onerror(app)
-
-
-const session = Koa_Session(session_config, app)
-app.keys = ["some secret hurr"];
-app.use(session)
-
-
-// middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger("dev"))
-
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
-
-
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// version check
+exec('npm info NeteaseCloudMusicApi version', (err, stdout, stderr) => {
+  if (!err) {
+    let version = stdout.trim()
+    if (packageJSON.version < version) {
+      console.log(
+        `最新版本: ${version}, 当前版本: ${packageJSON.version}, 请及时更新`,
+      )
+    }
+  }
 })
 
+const app = express()
+app.set('trust proxy', true)
 
-//初始化所有路由
-app.use(index.routes(), index.allowedMethods())
+// CORS & Preflight request
+app.use((req, res, next) => {
+  if (req.path !== '/' && !req.path.includes('.')) {
+    res.set({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8',
+    })
+  }
+  req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
 
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
+// cookie parser
+app.use((req, res, next) => {
+  req.cookies = {}
+  //;(req.headers.cookie || '').split(/\s*;\s*/).forEach((pair) => { //  Polynomial regular expression //
+  ;(req.headers.cookie || '').split(/;\s+|(?<!\s)\s+$/g).forEach((pair) => {
+    let crack = pair.indexOf('=')
+    if (crack < 1 || crack == pair.length - 1) return
+    req.cookies[decode(pair.slice(0, crack)).trim()] = decode(
+      pair.slice(crack + 1),
+    ).trim()
+  })
+  next()
+})
+
+// body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(fileUpload())
+
+// static
+app.use(express.static(path.join(__dirname, 'public')))
+
+// cache
+app.use(cache('2 minutes', (req, res) => res.statusCode === 200))
+// router
+const special = {
+  'daily_signin.js': '/daily_signin',
+  'fm_trash.js': '/fm_trash',
+  'personal_fm.js': '/personal_fm',
+}
+
+fs.readdirSync(path.join(__dirname, 'module'))
+  .reverse()
+  .forEach((file) => {
+    if (!file.endsWith('.js')) return
+    let route =
+      file in special
+        ? special[file]
+        : '/' + file.replace(/\.js$/i, '').replace(/_/g, '/')
+    let question = require(path.join(__dirname, 'module', file))
+
+    app.use(route, (req, res) => {
+      ;[req.query, req.body].forEach((item) => {
+        if (typeof item.cookie === 'string') {
+          item.cookie = cookieToJson(decode(item.cookie))
+        }
+      })
+      let query = Object.assign(
+        {},
+        { cookie: req.cookies },
+        req.query,
+        req.body,
+        req.files,
+      )
+
+      question(query, request)
+        .then((answer) => {
+          console.log('[OK]', decode(req.originalUrl))
+
+          const cookies = answer.cookie
+          if (Array.isArray(cookies) && cookies.length > 0) {
+            if (req.protocol === 'https') {
+              // Try to fix CORS SameSite Problem
+              res.append(
+                'Set-Cookie',
+                cookies.map((cookie) => {
+                  return cookie + '; SameSite=None; Secure'
+                }),
+              )
+            } else {
+              res.append('Set-Cookie', cookies)
+            }
+          }
+          res.status(answer.status).send(answer.body)
+        })
+        .catch((answer) => {
+          console.log('[ERR]', decode(req.originalUrl), {
+            status: answer.status,
+            body: answer.body,
+          })
+          if (!answer.body) {
+            res.status(404).send({
+              code: 404,
+              data: null,
+              msg: 'Not Found',
+            })
+            return
+          }
+          if (answer.body.code == '301') answer.body.msg = '需要登录'
+          res.append('Set-Cookie', answer.cookie)
+          res.status(answer.status).send(answer.body)
+        })
+    })
+  })
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || ''
+
+app.server = app.listen(port, host, () => {
+  console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
+})
 
 module.exports = app
--- models/tag.js
+++
@@ -1,7 +0,0 @@
-/**
- * Article models module.
- * @file 标签数据模型
- * @module models/tag
- * @author  lm
- */
-const { mongoose } = require('../config/mongodb');
